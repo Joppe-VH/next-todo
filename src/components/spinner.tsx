488712fc6aa32e4c851c3f6e00c65404d4b77f97
Joppe-VH <186156845+Joppe-VH@users.noreply.github.com>
2025-06-02 17:07:40 +0200

    make spinner take a classname

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,6 +1,8 @@
-const spinner = () => {
+const spinner = ({ className }: { className?: string }) => {
   return (
-    <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" />
+    <div
+      className={`w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin ${className}`}
+    />
   );
 };
 export default spinner;
